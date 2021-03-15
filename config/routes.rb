62effefc4d7ea8ac3ee89692389fc18bf34bdc2b
Matsu-pt <rx_77_3@icloud.com>
2021-03-15 19:07:37 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index" #massages_controller(コントローラー)のindexアクションを呼び出す
+  resources :users, only: [:edit, :update]
 end
