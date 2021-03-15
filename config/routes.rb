56ac156e20f2ec313316704a0861b6e94a221a62
Matsu-pt <rx_77_3@icloud.com>
2021-03-15 19:36:18 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index" #massages_controller(コントローラー)のindexアクションを呼び出す
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
